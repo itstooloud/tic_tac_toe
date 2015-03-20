59074409e885e853c9ed9094ae9f43473c640559
Chris Collingwood <itstooloud@gmail.com>
2015-03-19 23:44:36 -0400

    initial commit

---
+++ tic_tac_toe.rb
@@ -0,0 +1,37 @@
+=begin
+	
+Build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.
+
+    Think about how you would set up the different elements within the game... What should be a class? Instance variable? Method? A few minutes of thought can save you from wasting an hour of coding.
+    Build your game, taking care to not share information between classes any more than you have to.
+    Post your solution below, then check out the example solution provided.
+
+	
+=end
+
+grid = [] #two-dimensional array ?
+position = []
+#value = X or O
+
+class Player
+
+	def initialize(name)
+		@name = name
+	end
+
+	def move(player, :value, position)
+	end
+		
+end
+
+
+
+class Game
+
+
+	def solved?
+		
+	end
+
+end
+
